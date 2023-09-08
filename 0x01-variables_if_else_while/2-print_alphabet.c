94483040843ab6d61687977d8a71968be540c183
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-08 16:48:06 -0700

    My First commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point , no inputs needed
+ *
+ * Description:  A function that print alphabet in lowercase
+ *
+ * Return: 0 in success case
+ */
+
+int main(void)
+{
+	for (int i = 97 ; i <= 122 ; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
