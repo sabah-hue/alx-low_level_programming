5581087a6fbe8c47951fdb86f65d361604d90c6d
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-08 12:11:08 -0700

    My First commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point , no inputs needed
+ *
+ * Description:  A function that print with standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar10 - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
