d557220378172211284d0befe4f356d758516d88
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-07 18:18:18 -0700

    My First commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 /**
- * This is a c programmin function
- *
- * main: entry point , no inputs needed
+ * main - entry point , no inputs needed
  *
  * Description:  A function that print with puts
  *
- * return: 0 in success case
+ * Return: 0 in success case
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle"\");
 	return (0);
 }
