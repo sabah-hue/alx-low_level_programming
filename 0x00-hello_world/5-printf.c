db12db86067b842aa54b3a268c809f55d5e5d92a
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-07 18:32:49 -0700

    My First commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point , no inputs needed
+ *
+ * Description:  A function that print with printf
+ *
+ * Return: 0 in success case
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
