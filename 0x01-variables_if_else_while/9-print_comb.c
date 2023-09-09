d8de2f27ee46826fc4d70baea9439c33f60b7a04
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-08 18:44:00 -0700

    My First commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point , no inputs needed
+ *
+ * Description:  A function that print combinations of single-digit numbers
+ *
+ * Return: 0 in success case
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
