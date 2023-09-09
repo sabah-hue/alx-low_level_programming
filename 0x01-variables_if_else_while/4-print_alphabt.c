29040903790ae02ad2491dbad2df46ece0e1dd57
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-08 18:03:28 -0700

    My First commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point , no inputs needed
+ *
+ * Description:  A function that print alphabet in lowercase,  except e & q
+ *
+ * Return: 0 in success case
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 101 && i != 113)
+		putchar(i);
+	}
+	putchar('/n');
+	return (0);
+}
