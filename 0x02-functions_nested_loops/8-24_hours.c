1c7b3a1b8808e93060834a0895117fe4147679df
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-12 16:34:13 -0700

    My First commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+nclude "main.h"
+
+/**
+ * jack_bauer - entry point , no inputs needed
+ *
+ * Description:  A function that prints every minute of the day
+ *
+ * Return: no thing
+ */
+void jack_bauer(void)
+{
+	int m;
+	int s;
+
+	for (m = 00; m <= 23; m++)
+	{
+		for (s = 00; s <= 59; s++)
+		{
+			_putchar('m');
+			_putchar(':');
+			_putchar('s');
+			_putchar('\n');
+		}
+	}
+}
