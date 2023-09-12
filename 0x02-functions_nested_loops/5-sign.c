9bac9371fe57ccbaa7c68ee5b50b55a3953f535f
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-12 15:08:34 -0700

    My First commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point, need input
+ *
+ * Description: a function that prints the sign of a number.
+ * @n: the input to check
+ *
+ * Return: 1 input greater than 0,0 input is 0,-1 input less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (0);
+}
