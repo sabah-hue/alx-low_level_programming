1416d8ccddcf7391bde535d946b07c2fd026e0b0
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-12 16:19:09 -0700

    My First commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,12 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-
-	last_digit = n % 10;
+	
+	if (last_digit >= 10)
+	{
+		last_digit = n % 10;
+		return (last_digit);
+	}
+	last_digit = n;
 	return (last_digit);
 }
