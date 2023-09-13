3e6304939e02784cabe41c9df492f5cba8a83720
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-12 19:29:42 -0700

    My First commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,6 +18,10 @@ void print_to_98(int n)
 		for (i = n; i < 98; i++)
 		{
 			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 		_putchar('\n');
 	}
@@ -26,6 +30,10 @@ void print_to_98(int n)
 		for (i = n; i > 98; i--)
 		{
 			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 		_putchar('\n');
 	}
