0a2d67a45e172365a8604b44f016a84c989b6ed2
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-12 18:51:30 -0700

    My First commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ *
+ * Description:  A function that add two integer numbers
+ *@x: first parameter
+ *@y: second parameter
+ *
+ * Return: result of add two integers
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
