ebff4ea255fa65113aab561c8851f45a14ef7c05
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-12 17:59:50 -0700

    My First commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,6 +21,8 @@ void times_table(void)
 				_putchar(i * j + '0');
 				if (j != 9)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(',');
 				}
 			}
@@ -30,6 +32,7 @@ void times_table(void)
 				_putchar(i * j % 10 + '0');
 				if (j != 9)
 				{
+					_putchar(' ');
 					_putchar(',');
 				}
 			}
