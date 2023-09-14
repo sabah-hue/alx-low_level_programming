c4bfa93a76db89eff17a6ed4721b9cac1b870131
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-14 12:09:15 -0700

    My First commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+ * _isdigit - check the digits
+ *
+ *Description: cfunction to check the digits from 0 to 9
+ @c: input parameter
+ *
+ * Return:1 if digit , 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
