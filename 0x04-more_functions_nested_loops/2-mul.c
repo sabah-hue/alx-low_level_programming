055b8e7addd666aeefbe970f3c9074845d9d9ca5
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-14 12:21:24 -0700

    My First commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiply two numbers
+ *
+ * Description: a function that multiplies two integers.
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
