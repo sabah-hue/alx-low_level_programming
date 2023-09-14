d1acb62f448ae3f2b3ff8427f47cc5ed1533f97a
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-14 12:31:40 -0700

    My First commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * print_numbers - print 0 to 9
+ *
+ * Description: a function that prints the numbers, from 0 to 9
+ *
+ * Return: no thing.
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i = 48; i <= 56; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
