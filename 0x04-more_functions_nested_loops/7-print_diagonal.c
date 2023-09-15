688d6db153f2f4ce2f8278b09385c3cdb4a3afe1
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-14 17:21:02 -0700

    My First commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,10 +11,19 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 1; i <= n; i++)
 	{
-		_putchar(' ');
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(32);
+		}
+		_putchar(92);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
 }
