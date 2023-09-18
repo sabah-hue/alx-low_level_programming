7ff6783d153cefa99a9c57c42be6af20f3c59e49
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-17 21:11:09 -0700

    My First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change value
+ *
+ * Description: a function that  take apointer & change value
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
