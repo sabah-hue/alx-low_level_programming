34f62efeee0ccdc8d3022cebf1384e69c26bb747
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-18 14:55:38 -0700

    My First commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * Description: a function that reverse a string.
+ * @s: input string
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char x;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	i = i - 1;
+	for (j = 0; j < i / 2 ; j++)
+	{
+		x = s[j];
+		s[j] = s[i];
+		s[i--] = x;
+	}
+}
