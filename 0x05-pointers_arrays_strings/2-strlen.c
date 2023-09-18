8f6adf79ca463f67cfa4b0455698815755c0d59a
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-17 21:28:11 -0700

    My First commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - string length
+ *
+ * Description: a function that returns the length of a string.
+ * @s: input parameter
+ *
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	strlen(*s);
+}
