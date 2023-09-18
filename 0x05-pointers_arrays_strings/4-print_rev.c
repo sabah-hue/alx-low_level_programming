dde04da7c737a6214289aa991c609b1bb79b7c20
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-18 08:46:30 -0700

    My First commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,14 @@
 void print_rev(char *s)
 {
 	int i;
+	int y;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	}
-	while (s[i])
+	for (y = i - 1; y >= 0; y--)
 	{
-		_putchar(s[i]);
-		i--;
+		_putchar(s[y]);
 	}
 	_putchar('\n');
 }
