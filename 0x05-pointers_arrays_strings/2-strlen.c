94786da58c19b48c828ae302286bd9070dafcad5
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-17 22:04:40 -0700

    My First commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,9 +6,9 @@
  * Description: a function that returns the length of a string.
  * @s: input parameter
  *
- * Return: nothing
+ * Return: length
  */
 int _strlen(char *s)
 {
-	strlen(*s);
+	return (strlen(s));
 }
