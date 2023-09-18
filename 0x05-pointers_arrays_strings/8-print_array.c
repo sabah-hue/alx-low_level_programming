c849e89ceee32117b526b939c7559ad2037f14a7
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-18 16:45:10 -0700

    My First commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - print n elements of an array
+ *
+ * Description: a function that  print n elements of an array.
+ * @a: input array
+ * @n: input number of element
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d", a[n - 1]);
+}
