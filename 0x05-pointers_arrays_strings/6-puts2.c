455c0b7673ca7360bb4ef28ad69e5abaadb2b000
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-18 15:21:04 -0700

    My First commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,12 +3,18 @@
 /**
  * puts2 - print a string
  *
- * Description: a function that  prints every other character of a string, starting with the first character.
+ * Description: a function that  prints every other character of a string
  * @str: input string
  *
  * Return: nothing
  */
 void puts2(char *str)
 {
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i++]);
+	}
+	_putchar('\n');
 }
