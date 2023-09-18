daa6da01f7f32bf23161d9ae1992d172c4199ba7
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-18 16:23:48 -0700

    My First commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,19 +16,9 @@ void puts_half(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
-	if ((i - 1) % 2)
+	for (j = i / 2; j < i; j++)
 	{
-		for (j = (i - 1) / 2; j < i; j++)
-		{
-			_putchar(str[j]);
-		}
-	}
-	else
-	{
-		for (j = i / 2; j < i; j++)
-		{
-			_putchar(str[j]);
-		}
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
