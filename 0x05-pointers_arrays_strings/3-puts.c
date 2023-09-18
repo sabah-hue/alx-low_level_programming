d160cc3a26271f13cc1bef62f321a5e5c4f79ace
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-18 07:01:06 -0700

    My First commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ *
+ * Description: a function that prints string, followed by a new line.
+ * @str: input strring
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
