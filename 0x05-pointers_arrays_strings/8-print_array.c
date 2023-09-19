90b9b86b31a746f9f4288a2c2b15ad9f2cddefe6
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-18 17:00:54 -0700

    My First commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		if (i > 0)
+			printf(", %d", a[i]);
+		else
+			printf("%d", a[i]);
 	}
-	printf("%d\n", a[n - 1]);
+	printf("\n");
 }
