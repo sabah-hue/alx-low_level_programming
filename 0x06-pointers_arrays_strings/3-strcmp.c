ef18ae7db7cbf3c1d3c39e757a04935cca45559c
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-21 16:03:49 -0700

    My First commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,27 +7,12 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: 15 , -15 , 0
+ * Return: diff
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, diff;
+	int diff;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-	if (i > j)
-	{
-		diff = 15;
-	}
-	else if (i < j)
-	{
-		diff = -15;
-	}
-	else
-		diff = 0;
+	diff = *s1 - *s2;
 	return (diff);
 }
