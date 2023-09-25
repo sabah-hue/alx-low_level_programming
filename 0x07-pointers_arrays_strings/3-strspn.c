3fc5f2eaa786d4c35b5e77be8522235d5b1de50d
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-25 15:30:44 -0700

    My First commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; accept[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0' && s[j] != ','; j++)
+		for (j = 0; s[j] != '\0' && s[j] != ' '; j++)
 		{
 			if (accept[i] == s[j])
 			{
