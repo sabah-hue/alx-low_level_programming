96bbe63fc2e9ff37727ea7d4689554b3fdf2928c
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-25 16:29:46 -0700

    My First commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr -  locates a substring
+ *
+ * Description: a function that searches a string for any of a set of bytes.
+ * @haystack: input string
+ * @needle: substring
+ *
+ * Return:  a pointer to the beggining of substring,NULL if no substring.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int j;
+
+	for (j = 0; haystack[j] != '\0'; j++)
+	{
+		if (needle[j] != '\0')
+		{
+			if (needle[0] == haystack[j])
+			{
+				return (haystack + j);
+			}
+		}
+	}
+	return ('\0');
+}
