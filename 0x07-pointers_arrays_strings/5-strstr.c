d5f7c934198178a1c06ecb3da9728b4b4b35c33c
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-25 19:33:50 -0700

    My First commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,14 +17,14 @@ char *_strstr(char *haystack, char *needle)
 	{
 		if (haystack[i] == needle[0])
 		{
-			if (needle[j] == '\0')
-			{
-				return (haystack + i);
-			}
 			for (j = 0; needle[j] != '\0'; j++)
 			{
 				if (haystack[i + j] != needle[j])
 					break;
+			}
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
 			}
 		}
 	}
