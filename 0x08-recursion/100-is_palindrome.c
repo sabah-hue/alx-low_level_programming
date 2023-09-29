0bf822148981e8dddcdb0f0a8b7afb4792249ca2
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-28 19:37:23 -0700

    My First commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -24,6 +24,22 @@ int new_is_palindrome(char *s, int x, int i)
 	return (1);
 }
 /**
+ * _strlen_recursion -  get the length of a string
+ *
+ * Description: a function that  returns the length of a string.
+ * @s: input string
+ *
+ * Return: length of a string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
  * is_palindrome -  check a string is a palindrome.
  *
  * Description: a function return 1 if a string is a palindrome and 0 if not.
