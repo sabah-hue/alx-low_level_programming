7ae79cd954dbf0e267bf8f6fa717c898d5defc34
sabah-hue <sabah.abdelbaset@gmail.com>
2023-09-28 19:32:33 -0700

    My First commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,13 +1,40 @@
 #include "main.h"
 
 /**
- * int is_palindrome -  check a string is a palindrome.
+ * new_is_palindrome -  check a string is a palindrome.
  *
- * Description: a function that returns 1 if a string is a palindrome and 0 if not.
+ * Description: a function return 1 if a string is a palindrome and 0 if not.
+ * @s: input string
+ * @x: length of string
+ * @i: counter
+ *
+ * Return: 1 if a string a palindrome and 0 if not.
+ */
+int new_is_palindrome(char *s, int x, int i)
+{
+	if (i < (x / 2))
+	{
+		if (s[x] == s[i]);
+		new_is_palindrome(s, x--, i++);
+	}
+	else
+	{
+		return (0);
+	}
+	return (1);
+}
+/**
+ * is_palindrome -  check a string is a palindrome.
+ *
+ * Description: a function return 1 if a string is a palindrome and 0 if not.
  * @s: input string
  *
  * Return: 1 if a string a palindrome and 0 if not.
  */
 int is_palindrome(char *s)
 {
+	int x;
+
+	x = _strlen_recursion(s);
+	return (new_is_palindrome(s, x, 0));
 }
