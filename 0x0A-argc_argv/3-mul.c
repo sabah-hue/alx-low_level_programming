8feb46cedff1602eca2d08dc667771595bc0795a
root <root@DESKTOP-861G0OM.localdomain>
2023-10-02 17:06:58 +0300

    My First commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -  entry point.
+ *
+ * Description: a function that multiplies two numbers.
+ * @argc: variable store number of command line
+ * @argv: array of character
+ *
+ * Return: 0 in success.
+ **/
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc == 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = argc[1] * argc[2];
+		printf("%d\n");
+	}
+	return (0);
+
+}
