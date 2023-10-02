decbb082b68e18ba1e09717058ec157965b5e6e2
root <root@DESKTOP-861G0OM.localdomain>
2023-10-02 16:51:35 +0300

    My First commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  entry point.
+ *
+ * Description: a function  prints the number of arguments passed into it.
+ * @argc: variable store number of command line
+ * @argv: array of character
+ *
+ * Return: 0 in success.
+ **/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argv; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
