d31316ebbd5811e5eb01d2e4a6be34bf5db0c0c0
root <root@DESKTOP-861G0OM.localdomain>
2023-10-03 02:46:41 +0300

    My First commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,12 +3,57 @@
 /**
  * main -  entry point.
  *
- * Description: a function prints minimum number of coins to change an amount of money.
+ * Description: a function prints min number of coins to change a money.
  * @argc: variable store number of command line
- * * @argv: array of character
+ * @argv: array of character
  *
- * Return: 0 in success.
- *         **/
+ * Return: 0 in success, 1 in error.
+ **/
 int main(int argc, char *argv[])
 {
+	int a, count;
+
+	if (argc == 2)
+	{
+		a = atoi(argv[1]);
+		if (a > 0)
+		{
+			while (a > 0)
+			{
+				if (a - 25 > 0)
+				{
+					a = a - 25;
+					count++;
+				}
+				else if (a - 10 > 0 && a < 25)
+				{
+					a = a - 10;
+					count++;
+				}
+				else if (a - 5 > 0 && a < 10)
+				{
+					a = a - 5;
+					count++;
+				}
+				else if (a - 2 > 0 && a < 5)
+				{
+					a = a - 2;
+					count++;
+				}
+				else if (a - 1 > 0 && a < 2)
+				{
+					a = a - 1;
+					count++;
+				}
+			}
+			printf("%d\n", count);
+		}
+		printf("0\n");
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+	return (0);
 }
