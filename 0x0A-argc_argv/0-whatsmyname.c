51dbd5843ec6cf9ec2e521bbc3fe9aab4b1ebc1f
root <root@DESKTOP-861G0OM.localdomain>
2023-10-02 16:25:16 +0300

    My First commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,8 @@
  *
  * Return: 0 in success.
  **/
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	UNUSED(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
