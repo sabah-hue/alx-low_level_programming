dc2daf409eece7d6ea955f6785cfb7329359d465
root <root@DESKTOP-861G0OM.localdomain>
2023-10-02 15:58:25 +0300

    My First commit

---
+++ 0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -  entry point.
+ *
+ * Description: a function print its name followed by newline.
+ * @argc: variable store number of command line
+ * @argv: array of character
+ *
+ * Return: 0 in success.
+ **/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
