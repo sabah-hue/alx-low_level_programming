e8dc4e9ec83f74f5231407934ef4992912e5f1d0
root <root@DESKTOP-861G0OM.localdomain>
2023-10-05 05:01:42 +0300

    My First commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,11 +4,11 @@
 /**
  * alloc_grid - a pointer to a 2 dimensional array of integers.
  *
- * Description: a function returns a pointer to a 2 dimensional array of integers.
+ * Description: a function returns a pointer to a 2 D array..
  * @width: array width
  * @height: array height
  *
- * Return: a pointer to a 2 dimensional array, Null on failuer or negative dimentions
+ * Return: a pointer to a 2 D array, Null on failuer or -ve dimentions
  **/
 int **alloc_grid(int width, int height)
 {
@@ -26,6 +26,15 @@ int **alloc_grid(int width, int height)
 		{
 			for (j = 0; j < width; j++)
 				twoDarray[i][j] = 0;
+		}
+		else
+		{
+			while (i--)
+			{
+				free(twoDarray[i]);
+			}
+			free(twoDarray);
+			return (NULL);
 		}
 	}
 	return (twoDarray);
