4bcb27e801b6c196031e8dd24901c7a2672fc969
root <root@DESKTOP-861G0OM.localdomain>
2023-10-05 03:58:12 +0300

    My First commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,32 +23,20 @@ char *str_concat(char *s1, char *s2)
 	a = malloc((i + j) * sizeof(char) + 1);
 	if (a != NULL)
 	{
-		if (s1 == NULL && s2 != NULL)
-		{
-			for (j = 0; s2[j] != '\0'; j++)
-			{
-				a[j] = s2[j];
-			}
-		}
-		else if (s1 != NULL && s2 == NULL)
-		{
-			for (i = 0; s1[i] != '\0'; i++)
-			{
-				a[i] = s1[i];
-			}
-		}
-		else if (s1 == NULL && s2 == NULL)
+		if (s1 == NULL)
+			s1 = '\0';
+		if (s2 == NULL)
+			s2 = '\0';
+		if (s1 == NULL && s2 == NULL)
 			a = '\0';
-		else
+		for (z = 0; z < (i + j); z++)
 		{
-			for (z = 0; z < (i + j); z++)
-			{
-				if (i < z)
-					a[z] = s1[z];
-				else
-					a[z] = s2[z - i];
-			}
+			if (i < z)
+				a[z] = s1[z];
+			else
+				a[z] = s2[z - i];
 		}
+		a[z] = '\0';
 	}
 	return (a);
 }
