08824168be49dc5050bb29064b05e4269db63485
root <root@DESKTOP-861G0OM.localdomain>
2023-10-05 05:14:21 +0300

    My First commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		twoDarray[i] = malloc(sizeof(int) * width);
-		if (twoDarray != NULL)
+		if (twoDarray[i] != NULL)
 		{
 			for (j = 0; j < width; j++)
 				twoDarray[i][j] = 0;
