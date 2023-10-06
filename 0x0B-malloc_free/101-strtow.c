ec23ba89a7f4e37bd4de73a6011dd7c4f7ccaed2
root <root@DESKTOP-861G0OM.localdomain>
2023-10-06 05:10:33 +0300

    My First commit

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,33 @@
+#include <>
+
+/**
+ * strtow - splits a string into words.
+ *
+ * Description: a function that  that splits a string into words.
+ * @str: input array.
+ *
+ * Return: a pointer to array, or NULL if str == NULL or str == "".
+ **/
+char **strtow(char *str)
+{
+	int i, j;
+	char *a;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	for (i = 0; str != '\0'; i++)
+	{
+	}
+	a = malloc(sizeof(char) * i + 1);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; str != '\0'; i++)
+	{
+		for (j = 0; str != ' '; j++)
+		{
+			a[i][0] = str[j];
+		}
+		a[i++] = '\n';
+	}
+	return (a);
+}
