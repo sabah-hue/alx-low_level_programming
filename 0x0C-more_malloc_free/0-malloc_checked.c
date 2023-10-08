1ff47f11817d5d955cd52c430b826da59f895f49
root <root@DESKTOP-861G0OM.localdomain>
2023-10-08 04:08:17 +0300

    My First commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked -  allocates memory using malloc.
+ *
+ * Description: a function that allocates memory using malloc.
+ * @b: input array
+ *
+ * Return: pointer to allocated memory,
+ * in fail terminate with a status value of 98
+ **/
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *a;
+
+	a = malloc(sizeof(unsigned int) * b);
+	if (a != NULL)
+		return (a);
+	exit(98);
+}
