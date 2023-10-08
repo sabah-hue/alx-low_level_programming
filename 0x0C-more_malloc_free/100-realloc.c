94de3c632e9628b8513245d99a4a5dae27864401
root <root@DESKTOP-861G0OM.localdomain>
2023-10-09 01:37:26 +0300

    My First commit

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -45,8 +45,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		a = malloc(new_size);
-		if (a != NULL)
-			return (a);
-		return (NULL);
+		if (a == NULL)
+			return (NULL);
 	}
+	return (a);
 }
