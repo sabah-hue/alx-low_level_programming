f1051d4a9623b701ac21549a31586d106bab1454
root <root@DESKTOP-861G0OM.localdomain>
2023-10-08 21:42:42 +0300

    My First commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * _calloc -  allocate memory for an array.
+ *
+ * Description: a function that concatenates two strings.
+ * @nmemb: elements of  array
+ * @size: size bytes for each element
+ *
+ * Return: pointer to the allocated memory,
+ * in fail or nmemb or size is 0 return null
+ **/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *a;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc(size * nmemb);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		a[i] = 0;
+	}
+	return (a);
+}
