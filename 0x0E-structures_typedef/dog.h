50c5150c98323db9df9b229e190b0db37ca4886a
root <root@DESKTOP-861G0OM.localdomain>
2023-10-11 01:55:45 +0300

    My First commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,21 @@
  * @owner: owner data of the dog
  *
  * Description: Dog structure
- **/
-
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
+/**
+ * dog_t - Typedef for struct dog
+ **/
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
