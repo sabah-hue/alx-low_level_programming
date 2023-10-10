0bad288fac7baa49d3f1f1adb23f24461b786960
root <root@DESKTOP-861G0OM.localdomain>
2023-10-10 17:59:19 +0300

    My First commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - entry point. 
+ * main - entry point.
  *
- * Description: a function that prints the name of the file it was compiled from, followed by a new line.
+ * Description: a function that prints the name of file compiled from.
  *
- * Return: pointer to the newly created array, return NULL in fails or min > max
+ * Return: pointer to the newly created array,
+ * return NULL in fails or min > max
  **/
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
