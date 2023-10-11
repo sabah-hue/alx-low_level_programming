302adac0af24f5d8016710d2799a2c6c131b6b37
root <root@DESKTOP-861G0OM.localdomain>
2023-10-11 04:40:38 +0300

    My First commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,43 @@
 #include <stdlib.h>
 
 /**
+ * _strnlen - calculate length of string.
+ *
+ * Description: A function that return the length of a string.
+ * @s: String.
+ *
+ * Return: length of string.
+ **/
+int _strnlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (c);
+}
+/**
+ * _strncpy - Copy a string.
+ *
+ * Description: A function that copy a string.
+ * @dest: input string
+ * @src: Source string
+ *
+ * Return: a pointer to dest
+ **/
+char *_strncpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
+}
+/**
  * new_dog - creates a new dog.
  *
  * Description: a function that creates a new dog.
@@ -14,17 +51,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *Dog;
+	int ownerLen, nameLen;
 
+	if (name == NULL || owner == NULL || age < 0)
+		return (NULL);
 	Dog = malloc(sizeof(dog_t));
 	if (Dog == NULL)
 		return (NULL);
-	if (name == NULL || owner == NULL || age < 0)
+	nameLen = _strnlen(name);
+	ownerLen = _strnlen(owner);
+	Dog->name = malloc(sizeof(char *) * (nameLen + 1));
+	if (Dog->name == NULL)
 	{
-		free(Dog);
+		free(Dog->name);
 		return (NULL);
 	}
-	Dog->name = name;
+	Dog->owner = malloc(sizeof(char *) * (ownerLen + 1));
+	if (Dog->owner == NULL)
+		return (NULL);
+
+	Dog->name = _strncpy(Dog->name, name);
 	Dog->age = age;
-	Dog->owner = owner;
+	Dog->owner = _strncpy(Dog->owner, owner);
 	return (Dog);
 }
