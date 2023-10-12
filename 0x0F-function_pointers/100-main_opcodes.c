18ab1494d36b7d2c9456a4da4fe6427d34c45599
root <root@DESKTOP-861G0OM.localdomain>
2023-10-12 05:14:14 +0300

    My First commit

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "calc.h"
+
+/**
+ * main - Entry point.
+ *
+ * Description: a function that prints the opcodes of its own main function.
+ * @argc: count
+ * @argv: array of data
+ *
+ * Return: 0 in success.
+ **/
+int main(int argc, char *argv)
+{
+	int number_of_bytes, i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	number_of_bytes = atoi(argv[1]);
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	i = 0;
+	while (i < argc)
+	{
+
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
