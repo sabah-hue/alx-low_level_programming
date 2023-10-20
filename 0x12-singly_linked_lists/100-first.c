a4b62d694b2006c81cc200169f0fb729062bf93d
sabah-hue <sabah.abdelbaset@gmail.com>
2023-10-20 05:58:06 +0300

    My First commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+void executeFirst(void) __attribute__ ((constructor));
+/**
+ * executeFirst -  executed before main.
+ *
+ * Description: a function that run before main
+ *
+ * Return:  nothing
+ **/
+void executeFirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
