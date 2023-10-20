45279af9085d55592e60c3f426d07f8f68a5c8a8
sabah-hue <sabah.abdelbaset@gmail.com>
2023-10-20 04:19:06 +0300

    My First commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len -  returns the number of elements in a linked list_t list.
+ *
+ * Description: a function that the number of elements in a linked list_t list.
+ * @h: pointer to node.
+ *
+ * Return: the number of elements in a linked list_t list.
+ **/
+size_t list_len(const list_t *h)
+{
+	int i;
+	
+	for (i = 0; h != NULL; i++)
+	{
+		h->next;
+		i++;
+	}
+	return (i);
+}
