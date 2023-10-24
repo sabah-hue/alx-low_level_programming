6d5bc27b5ea032e8a1f400e4f27e66cb0ffbfa86
sabah-hue <sabah.abdelbaset@gmail.com>
2023-10-25 02:00:03 +0300

    My First commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,6 +18,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 	{
+		free(n);
 		free(newNode);
 		return (NULL);
 	}
@@ -33,5 +34,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 		*head = newNode;
 	newNode->next = NULL;
-	return (newNode);	
+	return (newNode);
 }
