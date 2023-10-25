880b072413ad78bd9465c39cdcf2cfe46e29a44f
sabah-hue <sabah.abdelbaset@gmail.com>
2023-10-25 04:54:35 +0300

    My First commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *addNode, *node, *tempNode;
 	unsigned int i;
 
-	if (head == NULL)
+	if (head == NULL || idx < 0)
 		return (NULL);
 	node = *head;
 	addNode = malloc(sizeof(listint_t));
