e5d11de84cbccb310102424aca45e4564256eb5e
sabah-hue <sabah.abdelbaset@gmail.com>
2023-10-25 04:52:01 +0300

    My First commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,6 +32,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (i == idx - 1)
 		{
+			if (node == NULL)
+				return (NULL);
 			tempNode = node->next;
 			node->next = addNode;
 			addNode->next = tempNode;
