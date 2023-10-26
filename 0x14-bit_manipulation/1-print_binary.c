a6ba857088070e42fc6a74ffd9789cb4b2b74108
sabah-hue <sabah.abdelbaset@gmail.com>
2023-10-27 00:45:52 +0200

    My First commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,14 +10,14 @@
  **/
 void print_binary(unsigned long int n)
 {
-	int i, c;
-	unsigned long int num_shifted;
-
-	c = 0;
-	for (i = sizeof(n) * 8; i >= 0; i--)
+	int i;
+	int c = 0;
+	
+	if (n == 0)
+		_putchar('0');
+	for (i = sizeof(n) * 4; i >= 0; i--)
 	{
-		num_hifted = n >> i;
-		if (num_shifted & 1)
+		if ((n >> i) & 1)
 		{
 			_putchar('1');
 			c++;
@@ -27,6 +27,4 @@ void print_binary(unsigned long int n)
 			_putchar('0');
 		}
 	}
-	if (c == 0)
-		_putchar('0');
 }
