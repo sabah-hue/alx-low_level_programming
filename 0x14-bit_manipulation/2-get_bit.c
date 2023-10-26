82be1e53f98f53178a00ee0b8e234cc3f1193f5d
sabah-hue <sabah.abdelbaset@gmail.com>
2023-10-27 01:06:05 +0200

    My First commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@
  **/
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (!n && index > (sizeof(n) * 8 - 1))
+	if (index > (sizeof(n) * 8 - 1))
 		return (-1);
 	return (n >> index & 1);
 }
