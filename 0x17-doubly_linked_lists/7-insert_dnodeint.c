dc313454b9649f5463e70695c61743ec735a03c4
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-09 04:35:47 +0200

    My First commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,6 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (node->prev)
 			node->prev->next = new_node;
 		node->prev = new_node;
+		return (new_node);
 	}
-	return (new_node);
+	return (null);
 }
