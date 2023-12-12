cbc230c95f2cf37a00f02991cf2b3636e9df1383
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-12 04:26:19 +0200

    My First commit

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - get the sum of two number
+ * @x: input number
+ * @y: input nuber
+ * Return: result the sum of two number
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+/**
+ * sub - get the diff between two numbers
+ * @x: input argument
+ * @y: input argument
+ * Return: result of subtract two numbers
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+/**
+ * mod - get mod
+ * @x: input arg
+ * @y: input arg
+ * Return: mod of two numbers
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
+/**
+ * mul - multibly
+ * @x: input arg
+ * @y: input arg
+ * Return: mul result
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+/**
+ * div - division
+ * @x: input arg
+ * @y: input arg
+ * Return: divition of two numbers
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
