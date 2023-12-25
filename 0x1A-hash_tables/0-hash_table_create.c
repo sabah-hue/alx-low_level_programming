c12afb7a9ea68bff25c55a9136d6e460bb597ac7
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-25 03:17:25 +0200

    My First commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,15 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *my_table;
-	unsigned int i;
+	unsigned int i = 0;
 
+	if (size < 1)
+		return (NULL);
 	my_table = malloc(sizeof(hash_table_t));
 	if (my_table == NULL)
 		return (NULL);
 	my_table->size = size;
-	my_table->array = malloc(sizeof(hash_table_t) * size);
+	my_table->array = malloc(sizeof(hash_node_t) * size);
 	if (my_table->array == NULL)
 	{
 		free(my_table);
