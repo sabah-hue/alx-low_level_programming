efd71c8fe4427c75d56caec30f38ecdd9f7156aa
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-25 03:09:04 +0200

    My First commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create hash table
+ *
+ * Description: A function that creat hash table
+ * @size: pointer to head node
+ *
+ * Return: pointer to the newly created hash table, NULL if some thing wrong
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *my_table;
+	unsigned int i;
+
+	my_table = malloc(sizeof(hash_table_t));
+	if (my_table == NULL)
+		return (NULL);
+	my_table->size = size;
+	my_table->array = malloc(sizeof(hash_table_t) * size);
+	if (my_table->array == NULL)
+	{
+		free(my_table);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		my_table->array[i] = NULL;
+		i++;
+	}
+	return (my_table);
+}
