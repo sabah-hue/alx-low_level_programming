71c1d45a39b81b1e7a36ea39837c502d6e9f63b2
sabah-hue <sabah.abdelbaset@gmail.com>
2023-12-25 04:35:35 +0200

    My First commit

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ *
+ * Description: A function that adds an element to the hash table.
+ * @ht: hash table you want to look into
+ * @key: key you are looking for
+ *
+ * Return:  value associated with the element, or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	i = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[i];
+	while (node)
+	{
+		if (!strcmp(node->key, key))
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
