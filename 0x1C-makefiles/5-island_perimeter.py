e257d76dd42f394ea2645323a8d7e15f4ed15851
sabah-hue <sabah.abdelbaset@gmail.com>
2024-04-08 01:53:50 +0200

    get preimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 """  returns the perimeter of the island described in grid """
+
+
 def island_perimeter(grid):
-    """ calculate perimeter """
-    
+    """
+    calculate perimeter
+    number of rows = len(grid)
+    number of cols = len(grid[0])
+    """
+    perimeter = 0
+    for i in len(grid):
+        for j in len(grid[0]):
+            if grid[i][j] == 1:
+                perimeter = perimeter + 4
+            if i < (len(grid) - 1) and grid[i + 1][j] == 1:
+                perimeter = perimeter - 2
+            if j < (len(grid[0]) - 1) and grid[i][j + 1] == 1:
+                perimeter = perimeter - 2
+    return perimeter
